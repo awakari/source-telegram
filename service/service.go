b34da758f583eb6a21797f2c2f6515d429d4377e
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-04-13 10:56:12 +0300

    feat: add found by query channels

--- service/service.go
+++ service/service.go
@@ -1,192 +1,251 @@
 package service
 
 import (
-    "context"
-    "errors"
-    "fmt"
-    "github.com/akurilov/go-tdlib/client"
-    "github.com/awakari/source-telegram/model"
-    "github.com/awakari/source-telegram/storage"
-    "log/slog"
-    "sync"
-    "time"
+	"context"
+	"errors"
+	"fmt"
+	"github.com/akurilov/go-tdlib/client"
+	"github.com/awakari/source-telegram/model"
+	"github.com/awakari/source-telegram/storage"
+	"github.com/awakari/source-telegram/util"
+	"log/slog"
+	"strings"
+	"sync"
+	"time"
 )
 
 type Service interface {
-    Create(ctx context.Context, ch model.Channel) (err error)
-    Read(ctx context.Context, link string) (ch model.Channel, err error)
-    Delete(ctx context.Context, link string) (err error)
-    GetPage(ctx context.Context, filter model.ChannelFilter, limit uint32, cursor string, order model.Order) (page []model.Channel, err error)
-    SearchAndAdd(ctx context.Context, groupId, subId, terms string, limit uint32) (n uint32, err error)
+	Create(ctx context.Context, ch model.Channel) (err error)
+	Read(ctx context.Context, link string) (ch model.Channel, err error)
+	Delete(ctx context.Context, link string) (err error)
+	GetPage(ctx context.Context, filter model.ChannelFilter, limit uint32, cursor string, order model.Order) (page []model.Channel, err error)
+	SearchAndAdd(ctx context.Context, groupId, subId, terms string, limit uint32) (n uint32, err error)
 
-    RefreshJoinedLoop() (err error)
+	RefreshJoinedLoop() (err error)
 }
 
 type service struct {
-    clientTg        *client.Client
-    stor            storage.Storage
-    chansJoined     map[int64]*model.Channel
-    chansJoinedLock *sync.Mutex
-    replicaRange    uint32
-    replicaIndex    uint32
-    log             *slog.Logger
+	clientTg        *client.Client
+	stor            storage.Storage
+	chansJoined     map[int64]*model.Channel
+	chansJoinedLock *sync.Mutex
+	replicaRange    uint32
+	replicaIndex    uint32
+	log             *slog.Logger
 }
 
 const ListLimit = 1_000
 const RefreshInterval = 5 * time.Minute
+const minTermLen = 4
+const minChanMemberCount = 100
 
 func NewService(
-    clientTg *client.Client,
-    stor storage.Storage,
-    chansJoined map[int64]*model.Channel,
-    chansJoinedLock *sync.Mutex,
-    replicaRange uint32,
-    replicaIndex uint32,
-    log *slog.Logger,
+	clientTg *client.Client,
+	stor storage.Storage,
+	chansJoined map[int64]*model.Channel,
+	chansJoinedLock *sync.Mutex,
+	replicaRange uint32,
+	replicaIndex uint32,
+	log *slog.Logger,
 ) Service {
-    return service{
-        clientTg:        clientTg,
-        stor:            stor,
-        chansJoined:     chansJoined,
-        chansJoinedLock: chansJoinedLock,
-        replicaRange:    replicaRange,
-        replicaIndex:    replicaIndex,
-        log:             log,
-    }
+	return service{
+		clientTg:        clientTg,
+		stor:            stor,
+		chansJoined:     chansJoined,
+		chansJoinedLock: chansJoinedLock,
+		replicaRange:    replicaRange,
+		replicaIndex:    replicaIndex,
+		log:             log,
+	}
 }
 
 func (svc service) Create(ctx context.Context, ch model.Channel) (err error) {
-    var newChat *client.Chat
-    newChat, err = svc.clientTg.SearchPublicChat(&client.SearchPublicChatRequest{
-        Username: ch.Link,
-    })
-    if err == nil {
-        if ch.Id == 0 {
-            ch.Id = newChat.Id
-        }
-        if ch.Name != newChat.Title {
-            ch.Name = newChat.Title
-        }
-        err = svc.stor.Create(ctx, ch)
-    }
-    return
+	var newChat *client.Chat
+	newChat, err = svc.clientTg.SearchPublicChat(&client.SearchPublicChatRequest{
+		Username: ch.Link,
+	})
+	if err == nil {
+		if ch.Id == 0 {
+			ch.Id = newChat.Id
+		}
+		if ch.Name != newChat.Title {
+			ch.Name = newChat.Title
+		}
+		ch.Created = time.Now().UTC()
+		ch.Last = ch.Created
+		err = svc.stor.Create(ctx, ch)
+	}
+	return
 }
 
 func (svc service) Read(ctx context.Context, link string) (ch model.Channel, err error) {
-    ch, err = svc.stor.Read(ctx, link)
-    return
+	ch, err = svc.stor.Read(ctx, link)
+	return
 }
 
 func (svc service) Delete(ctx context.Context, link string) (err error) {
-    err = svc.stor.Delete(ctx, link)
-    return
+	err = svc.stor.Delete(ctx, link)
+	return
 }
 
 func (svc service) GetPage(ctx context.Context, filter model.ChannelFilter, limit uint32, cursor string, order model.Order) (page []model.Channel, err error) {
-    page, err = svc.stor.GetPage(ctx, filter, limit, cursor, order)
-    return
+	page, err = svc.stor.GetPage(ctx, filter, limit, cursor, order)
+	return
 }
 
 func (svc service) RefreshJoinedLoop() (err error) {
-    ctx := context.TODO()
-    for err == nil {
-        err = svc.refreshJoined(ctx)
-        if err == nil {
-            time.Sleep(RefreshInterval)
-        }
-    }
-    return
+	ctx := context.TODO()
+	for err == nil {
+		err = svc.refreshJoined(ctx)
+		if err == nil {
+			time.Sleep(RefreshInterval)
+		}
+	}
+	return
 }
 
 func (svc service) refreshJoined(ctx context.Context) (err error) {
-    svc.log.Debug("Refresh joined channels started")
-    defer svc.log.Debug("Refresh joined channels finished")
-    // get all previously joined by the client chats
-    var chatsJoined *client.Chats
-    chatsJoined, err = svc.clientTg.GetChats(&client.GetChatsRequest{Limit: ListLimit})
-    var chans []model.Channel
-    if err == nil {
-        svc.log.Debug(fmt.Sprintf("Refresh joined channels: got %d from the client", len(chatsJoined.ChatIds)))
-        //
-        chanFilter := model.ChannelFilter{
-            IdDiv: svc.replicaRange,
-            IdRem: svc.replicaIndex,
-        }
-        chans, err = svc.stor.GetPage(ctx, chanFilter, ListLimit, "", model.OrderAsc) // it's important to get all at once
-    }
-    if err == nil {
-        svc.log.Debug(fmt.Sprintf("Refresh joined channels: got %d from the storage", len(chans)))
-        for _, ch := range chans {
-            var joined bool
-            for _, chatJoinedId := range chatsJoined.ChatIds {
-                if ch.Id == chatJoinedId {
-                    joined = true
-                    break
-                }
-            }
-            if !joined {
-                var newChat *client.Chat
-                newChat, err = svc.clientTg.SearchPublicChat(&client.SearchPublicChatRequest{
-                    Username: ch.Link,
-                })
-                svc.log.Debug(fmt.Sprintf("SearchPublicChat(%s): %+v, %s", ch.Name, newChat, err))
-                _, err = svc.clientTg.AddRecentlyFoundChat(&client.AddRecentlyFoundChatRequest{
-                    ChatId: ch.Id,
-                })
-                svc.log.Debug(fmt.Sprintf("AddRecentlyFoundChat(%d): %s", ch.Id, err))
-                _, err = svc.clientTg.JoinChat(&client.JoinChatRequest{
-                    ChatId: ch.Id,
-                })
-                if err == nil {
-                    joined = true
-                }
-            }
-            switch joined {
-            case true:
-                svc.log.Debug(fmt.Sprintf("Selected channel id: %d, title: %s, user: %s", ch.Id, ch.Name, ch.UserId))
-                svc.updateJoined(ctx, ch)
-            default:
-                svc.log.Warn(fmt.Sprintf("Failed to join channel by id: %d, cause: %s", ch.Id, err))
-                err = nil
-            }
-        }
-    }
-    return
+	svc.log.Debug("Refresh joined channels started")
+	defer svc.log.Debug("Refresh joined channels finished")
+	// get all previously joined by the client chats
+	var chatsJoined *client.Chats
+	chatsJoined, err = svc.clientTg.GetChats(&client.GetChatsRequest{Limit: ListLimit})
+	var chans []model.Channel
+	if err == nil {
+		svc.log.Debug(fmt.Sprintf("Refresh joined channels: got %d from the client", len(chatsJoined.ChatIds)))
+		//
+		chanFilter := model.ChannelFilter{
+			IdDiv: svc.replicaRange,
+			IdRem: svc.replicaIndex,
+		}
+		chans, err = svc.stor.GetPage(ctx, chanFilter, ListLimit, "", model.OrderAsc) // it's important to get all at once
+	}
+	if err == nil {
+		svc.log.Debug(fmt.Sprintf("Refresh joined channels: got %d from the storage", len(chans)))
+		for _, ch := range chans {
+			var joined bool
+			for _, chatJoinedId := range chatsJoined.ChatIds {
+				if ch.Id == chatJoinedId {
+					joined = true
+					break
+				}
+			}
+			if !joined {
+				var newChat *client.Chat
+				newChat, err = svc.clientTg.SearchPublicChat(&client.SearchPublicChatRequest{
+					Username: ch.Link,
+				})
+				svc.log.Debug(fmt.Sprintf("SearchPublicChat(%s): %+v, %s", ch.Name, newChat, err))
+				_, err = svc.clientTg.AddRecentlyFoundChat(&client.AddRecentlyFoundChatRequest{
+					ChatId: ch.Id,
+				})
+				svc.log.Debug(fmt.Sprintf("AddRecentlyFoundChat(%d): %s", ch.Id, err))
+				_, err = svc.clientTg.JoinChat(&client.JoinChatRequest{
+					ChatId: ch.Id,
+				})
+				if err == nil {
+					joined = true
+				}
+			}
+			switch joined {
+			case true:
+				svc.log.Debug(fmt.Sprintf("Selected channel id: %d, title: %s, user: %s", ch.Id, ch.Name, ch.UserId))
+				svc.updateJoined(ctx, ch)
+			default:
+				svc.log.Warn(fmt.Sprintf("Failed to join channel by id: %d, cause: %s", ch.Id, err))
+				err = nil
+			}
+		}
+	}
+	return
 }
 
 func (svc service) updateJoined(ctx context.Context, ch model.Channel) {
-    svc.chansJoinedLock.Lock()
-    defer svc.chansJoinedLock.Unlock()
-    chRuntime := svc.chansJoined[ch.Id]
-    switch chRuntime {
-    case nil:
-        svc.chansJoined[ch.Id] = &ch
-    default:
-        if chRuntime.Last.After(ch.Last) {
-            err := svc.stor.Update(ctx, ch.Link, ch.Last)
-            if err != nil {
-                svc.log.Warn(fmt.Sprintf("Failed to update the channel %s last update time, cause: %s", ch.Link, err))
-            }
-        }
-    }
-    return
+	svc.chansJoinedLock.Lock()
+	defer svc.chansJoinedLock.Unlock()
+	chRuntime := svc.chansJoined[ch.Id]
+	switch chRuntime {
+	case nil:
+		svc.chansJoined[ch.Id] = &ch
+	default:
+		if chRuntime.Last.After(ch.Last) {
+			err := svc.stor.Update(ctx, ch.Link, ch.Last)
+			if err != nil {
+				svc.log.Warn(fmt.Sprintf("Failed to update the channel %s last update time, cause: %s", ch.Link, err))
+			}
+		}
+	}
+	return
 }
 
 func (svc service) SearchAndAdd(ctx context.Context, groupId, subId, terms string, limit uint32) (n uint32, err error) {
-    var chats *client.Chats
-    chats, err = svc.clientTg.SearchPublicChats(&client.SearchPublicChatsRequest{
-        Query: terms,
-    })
-    if err == nil && chats != nil {
-        for _, chatId := range chats.ChatIds {
-            var chat *client.Chat
-            var chatErr error
-            chat, chatErr = svc.clientTg.GetChat(&client.GetChatRequest{
-                ChatId: chatId,
-            })
-            fmt.Printf("SearchAndAdd(%s): chat %+v, err: %s\n", terms, chat, chatErr)
-            err = errors.Join(err, chatErr)
-        }
-    }
-    return
+	terms = util.Sanitize(terms)
+	termSet := map[string]bool{}
+	for _, t := range strings.Split(terms, " ") {
+		if len(t) >= minTermLen {
+			termSet[t] = true
+		}
+	}
+	var tn uint32
+	var tErr error
+	for t, _ := range termSet {
+		tn, tErr = svc.searchAndAdd(ctx, subId, groupId, t, limit)
+		n += tn
+		err = errors.Join(err, tErr)
+	}
+	return
+}
+
+func (svc service) searchAndAdd(ctx context.Context, groupId, subId, term string, limit uint32) (n uint32, err error) {
+	var chats *client.Chats
+	chats, err = svc.clientTg.SearchPublicChats(&client.SearchPublicChatsRequest{
+		Query: term,
+	})
+	if err == nil && chats != nil {
+		for i, chatId := range chats.ChatIds {
+			if i >= int(limit) {
+				break
+			}
+			var chat *client.Chat
+			var chatErr error
+			chat, chatErr = svc.clientTg.GetChat(&client.GetChatRequest{
+				ChatId: chatId,
+			})
+			var sg *client.Supergroup
+			if chatErr == nil && chat.Type.ChatTypeType() == client.TypeChatTypeSupergroup {
+				sgChat := chat.Type.(*client.ChatTypeSupergroup)
+				if sgChat.IsChannel {
+					sg, chatErr = svc.clientTg.GetSupergroup(&client.GetSupergroupRequest{
+						SupergroupId: sgChat.SupergroupId,
+					})
+				}
+			}
+			if chatErr == nil && sg != nil {
+				var name string
+				if sg.Usernames != nil && len(sg.Usernames.ActiveUsernames) > 0 {
+					name = sg.Usernames.ActiveUsernames[0]
+				}
+				if name != "" && !sg.IsScam && !sg.IsFake && sg.MemberCount > minChanMemberCount {
+					now := time.Now().UTC()
+					chatErr = svc.stor.Create(ctx, model.Channel{
+						Id:      chatId,
+						GroupId: groupId,
+						Name:    fmt.Sprintf("%+v", sg.Usernames),
+						Link:    "@" + name,
+						SubId:   subId,
+						Terms:   term,
+						Created: now,
+						Last:    now,
+					})
+				}
+			}
+			switch chatErr {
+			case nil:
+				n++
+			default:
+				err = errors.Join(err, chatErr)
+			}
+		}
+	}
+	return
 }
--- storage/storage_mongo.go
+++ storage/storage_mongo.go
@@ -1,28 +1,28 @@
 package storage
 
 import (
-    "context"
-    "crypto/tls"
-    "errors"
-    "fmt"
-    "github.com/awakari/source-telegram/config"
-    "github.com/awakari/source-telegram/model"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "time"
+	"context"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"github.com/awakari/source-telegram/config"
+	"github.com/awakari/source-telegram/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
 )
 
 type recChan struct {
-    Id      int64     `bson:"id"`
-    GroupId string    `bson:"groupId"`
-    UserId  string    `bson:"userId"`
-    Name    string    `bson:"name"`
-    Link    string    `bson:"link"`
-    Created time.Time `bson:"created"`
-    Last    time.Time `bson:"last"`
-    SubId   string    `bson:"subId"`
-    Terms   string    `bson:"terms"`
+	Id      int64     `bson:"id"`
+	GroupId string    `bson:"groupId"`
+	UserId  string    `bson:"userId,omitempty"`
+	Name    string    `bson:"name"`
+	Link    string    `bson:"link"`
+	Created time.Time `bson:"created,omitempty"`
+	Last    time.Time `bson:"last,omitempty"`
+	SubId   string    `bson:"subId,omitempty"`
+	Terms   string    `bson:"terms,omitempty"`
 }
 
 const attrId = "id"
@@ -36,302 +36,302 @@ const attrSubId = "subId"
 const attrTerms = "terms"
 
 type storageMongo struct {
-    conn *mongo.Client
-    db   *mongo.Database
-    coll *mongo.Collection
+	conn *mongo.Client
+	db   *mongo.Database
+	coll *mongo.Collection
 }
 
 var optsSrvApi = options.ServerAPI(options.ServerAPIVersion1)
 var optsGet = options.
-    FindOne().
-    SetShowRecordID(false).
-    SetProjection(projGet)
+	FindOne().
+	SetShowRecordID(false).
+	SetProjection(projGet)
 var projGet = bson.D{
-    {
-        Key:   attrId,
-        Value: 1,
-    },
-    {
-        Key:   attrGroupId,
-        Value: 1,
-    },
-    {
-        Key:   attrUserId,
-        Value: 1,
-    },
-    {
-        Key:   attrName,
-        Value: 1,
-    },
-    {
-        Key:   attrLink,
-        Value: 1,
-    },
-    {
-        Key:   attrCreated,
-        Value: 1,
-    },
-    {
-        Key:   attrLast,
-        Value: 1,
-    },
-    {
-        Key:   attrSubId,
-        Value: 1,
-    },
-    {
-        Key:   attrTerms,
-        Value: 1,
-    },
+	{
+		Key:   attrId,
+		Value: 1,
+	},
+	{
+		Key:   attrGroupId,
+		Value: 1,
+	},
+	{
+		Key:   attrUserId,
+		Value: 1,
+	},
+	{
+		Key:   attrName,
+		Value: 1,
+	},
+	{
+		Key:   attrLink,
+		Value: 1,
+	},
+	{
+		Key:   attrCreated,
+		Value: 1,
+	},
+	{
+		Key:   attrLast,
+		Value: 1,
+	},
+	{
+		Key:   attrSubId,
+		Value: 1,
+	},
+	{
+		Key:   attrTerms,
+		Value: 1,
+	},
 }
 var sortGetBatchAsc = bson.D{
-    {
-        Key:   attrLink,
-        Value: 1,
-    },
+	{
+		Key:   attrLink,
+		Value: 1,
+	},
 }
 var sortGetBatchDesc = bson.D{
-    {
-        Key:   attrLink,
-        Value: -1,
-    },
+	{
+		Key:   attrLink,
+		Value: -1,
+	},
 }
 
 func NewStorage(ctx context.Context, cfgDb config.DbConfig) (s Storage, err error) {
-    clientOpts := options.
-        Client().
-        ApplyURI(cfgDb.Uri).
-        SetServerAPIOptions(optsSrvApi)
-    if cfgDb.Tls.Enabled {
-        clientOpts = clientOpts.SetTLSConfig(&tls.Config{InsecureSkipVerify: cfgDb.Tls.Insecure})
-    }
-    if len(cfgDb.UserName) > 0 {
-        auth := options.Credential{
-            Username:    cfgDb.UserName,
-            Password:    cfgDb.Password,
-            PasswordSet: len(cfgDb.Password) > 0,
-        }
-        clientOpts = clientOpts.SetAuth(auth)
-    }
-    conn, err := mongo.Connect(ctx, clientOpts)
-    var sm storageMongo
-    if err == nil {
-        db := conn.Database(cfgDb.Name)
-        coll := db.Collection(cfgDb.Table.Name)
-        sm.conn = conn
-        sm.db = db
-        sm.coll = coll
-        _, err = sm.ensureIndices(ctx, cfgDb.Table.Retention)
-    }
-    if err == nil {
-        s = sm
-    }
-    return
+	clientOpts := options.
+		Client().
+		ApplyURI(cfgDb.Uri).
+		SetServerAPIOptions(optsSrvApi)
+	if cfgDb.Tls.Enabled {
+		clientOpts = clientOpts.SetTLSConfig(&tls.Config{InsecureSkipVerify: cfgDb.Tls.Insecure})
+	}
+	if len(cfgDb.UserName) > 0 {
+		auth := options.Credential{
+			Username:    cfgDb.UserName,
+			Password:    cfgDb.Password,
+			PasswordSet: len(cfgDb.Password) > 0,
+		}
+		clientOpts = clientOpts.SetAuth(auth)
+	}
+	conn, err := mongo.Connect(ctx, clientOpts)
+	var sm storageMongo
+	if err == nil {
+		db := conn.Database(cfgDb.Name)
+		coll := db.Collection(cfgDb.Table.Name)
+		sm.conn = conn
+		sm.db = db
+		sm.coll = coll
+		_, err = sm.ensureIndices(ctx, cfgDb.Table.Retention)
+	}
+	if err == nil {
+		s = sm
+	}
+	return
 }
 
 func (sm storageMongo) ensureIndices(ctx context.Context, retentionPeriod time.Duration) ([]string, error) {
-    return sm.coll.Indexes().CreateMany(ctx, []mongo.IndexModel{
-        {
-            Keys: bson.D{
-                {
-                    Key:   attrId,
-                    Value: 1,
-                },
-            },
-            Options: options.
-                Index().
-                SetUnique(true),
-        },
-        {
-            Keys: bson.D{
-                {
-                    Key:   attrLink,
-                    Value: 1,
-                },
-            },
-            Options: options.
-                Index().
-                SetUnique(true),
-        },
-        {
-            Keys: bson.D{
-                {
-                    Key:   attrLast,
-                    Value: 1,
-                },
-            },
-            Options: options.
-                Index().
-                SetExpireAfterSeconds(int32(retentionPeriod / time.Second)).
-                SetUnique(false),
-        },
-    })
+	return sm.coll.Indexes().CreateMany(ctx, []mongo.IndexModel{
+		{
+			Keys: bson.D{
+				{
+					Key:   attrId,
+					Value: 1,
+				},
+			},
+			Options: options.
+				Index().
+				SetUnique(true),
+		},
+		{
+			Keys: bson.D{
+				{
+					Key:   attrLink,
+					Value: 1,
+				},
+			},
+			Options: options.
+				Index().
+				SetUnique(true),
+		},
+		{
+			Keys: bson.D{
+				{
+					Key:   attrLast,
+					Value: 1,
+				},
+			},
+			Options: options.
+				Index().
+				SetExpireAfterSeconds(int32(retentionPeriod / time.Second)).
+				SetUnique(false),
+		},
+	})
 }
 
 func (sm storageMongo) Close() error {
-    return sm.conn.Disconnect(context.TODO())
+	return sm.conn.Disconnect(context.TODO())
 }
 
 func (sm storageMongo) Create(ctx context.Context, ch model.Channel) (err error) {
-    rec := recChan{
-        Id:      ch.Id,
-        GroupId: ch.GroupId,
-        UserId:  ch.UserId,
-        Name:    ch.Name,
-        Link:    ch.Link,
-        Created: ch.Created,
-    }
-    _, err = sm.coll.InsertOne(ctx, rec)
-    err = decodeError(err, ch.Link)
-    return
+	rec := recChan{
+		Id:      ch.Id,
+		GroupId: ch.GroupId,
+		UserId:  ch.UserId,
+		Name:    ch.Name,
+		Link:    ch.Link,
+		Created: ch.Created,
+	}
+	_, err = sm.coll.InsertOne(ctx, rec)
+	err = decodeError(err, ch.Link)
+	return
 }
 
 func (sm storageMongo) Read(ctx context.Context, link string) (ch model.Channel, err error) {
-    q := bson.M{
-        attrLink: link,
-    }
-    var result *mongo.SingleResult
-    result = sm.coll.FindOne(ctx, q, optsGet)
-    err = result.Err()
-    var rec recChan
-    if err == nil {
-        err = result.Decode(&rec)
-    }
-    if err == nil {
-        ch.Id = rec.Id
-        ch.GroupId = rec.GroupId
-        ch.UserId = rec.UserId
-        ch.Name = rec.Name
-        ch.Link = rec.Link
-        ch.Created = rec.Created
-        ch.Last = rec.Last
-        ch.SubId = rec.SubId
-        ch.Terms = rec.Terms
-    }
-    err = decodeError(err, link)
-    return
+	q := bson.M{
+		attrLink: link,
+	}
+	var result *mongo.SingleResult
+	result = sm.coll.FindOne(ctx, q, optsGet)
+	err = result.Err()
+	var rec recChan
+	if err == nil {
+		err = result.Decode(&rec)
+	}
+	if err == nil {
+		ch.Id = rec.Id
+		ch.GroupId = rec.GroupId
+		ch.UserId = rec.UserId
+		ch.Name = rec.Name
+		ch.Link = rec.Link
+		ch.Created = rec.Created
+		ch.Last = rec.Last
+		ch.SubId = rec.SubId
+		ch.Terms = rec.Terms
+	}
+	err = decodeError(err, link)
+	return
 }
 
 func (sm storageMongo) Update(ctx context.Context, link string, last time.Time) (err error) {
-    q := bson.M{
-        attrLink: link,
-    }
-    u := bson.M{
-        "$set": bson.M{
-            attrLast: last.UTC(),
-        },
-    }
-    var result *mongo.UpdateResult
-    result, err = sm.coll.UpdateOne(ctx, q, u)
-    switch err {
-    case nil:
-        if result.MatchedCount < 1 {
-            err = fmt.Errorf("%w by link %s", ErrNotFound, link)
-        }
-    default:
-        err = decodeError(err, link)
-    }
-    return
+	q := bson.M{
+		attrLink: link,
+	}
+	u := bson.M{
+		"$set": bson.M{
+			attrLast: last.UTC(),
+		},
+	}
+	var result *mongo.UpdateResult
+	result, err = sm.coll.UpdateOne(ctx, q, u)
+	switch err {
+	case nil:
+		if result.MatchedCount < 1 {
+			err = fmt.Errorf("%w by link %s", ErrNotFound, link)
+		}
+	default:
+		err = decodeError(err, link)
+	}
+	return
 }
 
 func (sm storageMongo) Delete(ctx context.Context, link string) (err error) {
-    q := bson.M{
-        attrLink: link,
-    }
-    var result *mongo.DeleteResult
-    result, err = sm.coll.DeleteOne(ctx, q)
-    switch err {
-    case nil:
-        if result.DeletedCount < 1 {
-            err = fmt.Errorf("%w by link %s", ErrNotFound, link)
-        }
-    default:
-        err = decodeError(err, link)
-    }
-    return
+	q := bson.M{
+		attrLink: link,
+	}
+	var result *mongo.DeleteResult
+	result, err = sm.coll.DeleteOne(ctx, q)
+	switch err {
+	case nil:
+		if result.DeletedCount < 1 {
+			err = fmt.Errorf("%w by link %s", ErrNotFound, link)
+		}
+	default:
+		err = decodeError(err, link)
+	}
+	return
 }
 
 func (sm storageMongo) GetPage(ctx context.Context, filter model.ChannelFilter, limit uint32, cursor string, order model.Order) (page []model.Channel, err error) {
-    q := bson.M{}
-    if filter.IdDiv != 0 {
-        q[attrId] = bson.M{
-            "$mod": bson.A{
-                filter.IdDiv,
-                -int32(filter.IdRem),
-            },
-        }
-    }
-    if filter.UserId != "" {
-        q[attrGroupId] = filter.GroupId
-        q[attrUserId] = filter.UserId
-    }
-    optsList := options.
-        Find().
-        SetLimit(int64(limit)).
-        SetShowRecordID(false).
-        SetProjection(projGet)
-    var clauseCursor bson.M
-    switch order {
-    case model.OrderDesc:
-        clauseCursor = bson.M{
-            "$lt": cursor,
-        }
-        optsList = optsList.SetSort(sortGetBatchDesc)
-    default:
-        clauseCursor = bson.M{
-            "$gt": cursor,
-        }
-        optsList = optsList.SetSort(sortGetBatchAsc)
-    }
-    q["$and"] = []bson.M{
-        {
-            attrLink: clauseCursor,
-        },
-        {
-            "$or": []bson.M{
-                {
-                    attrLink: bson.M{
-                        "$regex": filter.Pattern,
-                    },
-                },
-                {
-                    attrName: bson.M{
-                        "$regex": filter.Pattern,
-                    },
-                },
-            },
-        },
-    }
-    var cur *mongo.Cursor
-    cur, err = sm.coll.Find(ctx, q, optsList)
-    if err == nil {
-        for cur.Next(ctx) {
-            var rec recChan
-            err = errors.Join(err, cur.Decode(&rec))
-            if err == nil {
-                page = append(page, model.Channel{
-                    Id:      rec.Id,
-                    GroupId: rec.GroupId,
-                    UserId:  rec.UserId,
-                    Name:    rec.Name,
-                    Link:    rec.Link,
-                })
-            }
-        }
-    }
-    err = decodeError(err, cursor)
-    return
+	q := bson.M{}
+	if filter.IdDiv != 0 {
+		q[attrId] = bson.M{
+			"$mod": bson.A{
+				filter.IdDiv,
+				-int32(filter.IdRem),
+			},
+		}
+	}
+	if filter.UserId != "" {
+		q[attrGroupId] = filter.GroupId
+		q[attrUserId] = filter.UserId
+	}
+	optsList := options.
+		Find().
+		SetLimit(int64(limit)).
+		SetShowRecordID(false).
+		SetProjection(projGet)
+	var clauseCursor bson.M
+	switch order {
+	case model.OrderDesc:
+		clauseCursor = bson.M{
+			"$lt": cursor,
+		}
+		optsList = optsList.SetSort(sortGetBatchDesc)
+	default:
+		clauseCursor = bson.M{
+			"$gt": cursor,
+		}
+		optsList = optsList.SetSort(sortGetBatchAsc)
+	}
+	q["$and"] = []bson.M{
+		{
+			attrLink: clauseCursor,
+		},
+		{
+			"$or": []bson.M{
+				{
+					attrLink: bson.M{
+						"$regex": filter.Pattern,
+					},
+				},
+				{
+					attrName: bson.M{
+						"$regex": filter.Pattern,
+					},
+				},
+			},
+		},
+	}
+	var cur *mongo.Cursor
+	cur, err = sm.coll.Find(ctx, q, optsList)
+	if err == nil {
+		for cur.Next(ctx) {
+			var rec recChan
+			err = errors.Join(err, cur.Decode(&rec))
+			if err == nil {
+				page = append(page, model.Channel{
+					Id:      rec.Id,
+					GroupId: rec.GroupId,
+					UserId:  rec.UserId,
+					Name:    rec.Name,
+					Link:    rec.Link,
+				})
+			}
+		}
+	}
+	err = decodeError(err, cursor)
+	return
 }
 
 func decodeError(src error, link string) (dst error) {
-    switch {
-    case src == nil:
-    case errors.Is(src, mongo.ErrNoDocuments):
-        dst = fmt.Errorf("%w: %s", ErrNotFound, link)
-    case mongo.IsDuplicateKeyError(src):
-        dst = fmt.Errorf("%w: %s", ErrConflict, link)
-    default:
-        dst = fmt.Errorf("%w: %s", ErrInternal, src)
-    }
-    return
+	switch {
+	case src == nil:
+	case errors.Is(src, mongo.ErrNoDocuments):
+		dst = fmt.Errorf("%w: %s", ErrNotFound, link)
+	case mongo.IsDuplicateKeyError(src):
+		dst = fmt.Errorf("%w: %s", ErrConflict, link)
+	default:
+		dst = fmt.Errorf("%w: %s", ErrInternal, src)
+	}
+	return
 }
